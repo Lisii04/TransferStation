fca23a3e8504a2c558a05f7aef6943c6f1a7681b
Lisii_Alliance <2256106896@qq.com>
2023-09-26 22:58:33 +0800

    changePUSH.SH in 2023-09-26 22:58

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git reset .
 git add .
 if [ $1 ]
 then 
-    git commit -m "$1 Update in "$(date +%F)" "$(date +%R)
+    git commit -m "$1 in "$(date +%F)" "$(date +%R)
 else
     git commit -m "Update in "$(date +%F)" "$(date +%R)
 fi
