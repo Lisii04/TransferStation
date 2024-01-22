9e447ee19fcce074a07822cbcd9b5681ef727a1d
Lisii04 <2256106896@qq.com>
2024-01-22 16:11:38 +0800

    Update in 2024-01-22 16:11

---
+++ 1_Projects/Current/Train-travel-distance/Train-opencv/build/CMakeFiles/Train-opencv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Train-opencv.dir/Src/positive_flow.cpp.o"
+  "CMakeFiles/Train-opencv.dir/Src/positive_flow.cpp.o.d"
+  "Train-opencv"
+  "Train-opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Train-opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
